2ea9cecc2c66ea1a7a3055948d1e221a2171a15d
Ellis Mok <ellismok1@gmail.com>
2021-06-09 19:34:28 -0700

    adding files and changes

--- static/js/logic.js
+++ static/js/logic.js
@@ -86,10 +86,6 @@ L.control
   .addTo(allMaps);
 
 
-
-
-
-
 // Use d3.json() to call the API endpoint for earthquake geoJSON data, 
 // .then() fire off an anonymous function that takes a single argument `data`.
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
@@ -97,7 +93,6 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.g
 d3.json(url).then(function(data) {
   var earthquakeData = data.features;
   // console.log(earthquakeData)
-}
 
 // This function determines the color of the marker based on the magnitude of the earthquake.
 function getColor(intensity) {
@@ -130,22 +125,13 @@ L.geoJson(data, {
   // We turn each feature into a circleMarker on the map.
   pointToLayer: function(feature, latlng) {
     return L.circleMarker(latlng, {
-      radius: 10, 
-      fillOpacity: 0.85
+      radius: getRadius(feature.properties.mag), 
+      color: 'red',
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      fillOpacity: 0.5,
     });
   },
-  // We set the style for each circleMarker using our styleInfo function.
-  style: function (styleInfo) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      getColor: getColor(feature.geometry.coordinates[2]), // grabbing the coordinates from the json (e.g. "coordinates":[-74.9491,-39.2503,11.76])
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),// grabbing the magnitude (e.g. "mag":4.3) from json 
-      stroke: true,
-      weight: 0.5
-    };,
-  },
+
   // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
   onEachFeature: function(feature, layer) {
     layer.bindPopup(
@@ -160,7 +146,7 @@ L.geoJson(data, {
 }).addTo(earthquakes);
 
 earthquakes.addTo(allMaps);
-});
+
 
 // Here we create a legend control object.
 var legend = L.control({
@@ -173,12 +159,12 @@ legend.onAdd = function() {
 
   var grades = [-10, 10, 30, 50, 70, 90];
   var colors = [
-    "#98ee00",
-    "#d4ee00",
-    "#eecc00",
-    "#ee9c00",
-    "#ea822c",
-    "#ea2c2c"
+    "#b71212",
+    "#e61717",
+    "#ea2c2c",
+    "#ec4343",
+    "#ef5a5a",
+    "#f17272"
   ];
 
 // loop through our density intervals and generate a label with a colored square for each interval
@@ -189,7 +175,9 @@ legend.onAdd = function() {
   return div;
 };
 // Finally, we our legend to the map.
-legend.addTo(myMaps);
+legend.addTo(allMaps);
+
+});
 
   // // BONUS
   // // Make another d3.json() call to the tectonic plates API endpoint
@@ -202,4 +190,4 @@ legend.addTo(myMaps);
   //     // Then add the tectonicplates layer to the map.
   //     YOUR_CODE_HERE; // use .addTo to add the tectonicPlates LayerGroup to the myMap object
   //   });
-});
+// });
